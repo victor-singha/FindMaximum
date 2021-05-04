861cfa035a52945da918b6f917246795e5a8336d
victor-singha <vicunite2@gmail.com>
2021-05-04 14:23:55 +0530

    [victor] max string

--- FindMaximum/Program.cs
+++ FindMaximum/Program.cs
@@ -11,25 +11,34 @@ namespace FindMaximum
         static void Main(string[] args)
         {
 
-            int maxInt = findMaxumum(1, 2, 3);
-            float maxFloat = findMaxumum(1.1f, 9.4f, 5.7f);
+            int maxInt = findMaximum(1, 2, 3);
+            float maxFloat = findMaximum(1.1f, 9.4f, 5.7f);
+            string maxString = findMaximum("Apple", "Peach", "Banana");
             Console.WriteLine(maxInt);
             Console.WriteLine(maxFloat);
+            Console.WriteLine(maxString);
             Console.ReadKey();
             
         }
-        static int findMaxumum(int num1, int num2, int num3)
+        static int findMaximum(int num1, int num2, int num3)
         {
             if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0) return num1;
             else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0) return num2;
             else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0) return num3;
             else throw new Exception("Values are same");
         }
-        static float findMaxumum(float num1, float num2, float num3)
+        static float findMaximum(float num1, float num2, float num3)
         {
             if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0) return num1;
             else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0) return num2;
             else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0) return num3;
+            else throw new Exception("Values are same");
+        }
+        static string findMaximum(string input1, string input2, string input3)
+        {
+            if (input1.CompareTo(input2) > 0 && input1.CompareTo(input3) > 0) return input1;
+            else if (input2.CompareTo(input1) > 0 && input2.CompareTo(input3) > 0) return input2;
+            else if (input3.CompareTo(input1) > 0 && input3.CompareTo(input2) > 0) return input3;
             else throw new Exception("Values are same");
         }
 
