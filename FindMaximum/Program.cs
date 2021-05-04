b72032ae81800d5b1d74b69933fbb4a4347b61cb
victor-singha <vicunite2@gmail.com>
2021-05-04 14:15:07 +0530

    initial commit

---
+++ FindMaximum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 6;
+            int num2 = 8;
+            int num3 = 3;
+            int max = findMaxumum(num1, num2, num3);
+            Console.WriteLine(max);
+            Console.ReadKey();
+            
+        }
+        static int findMaxumum(int num1, int num2, int num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0) return num1;
+            else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0) return num2;
+            else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0) return num3;
+            else throw new Exception("Values are same");
+        }
+
+
+    }
+}
