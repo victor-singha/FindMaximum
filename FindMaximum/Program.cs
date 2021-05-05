954e03ed7b911fc25bcbf6e48df6d7378059b80f
victor-singha <vicunite2@gmail.com>
2021-05-05 11:19:19 +0530

    [victor] refactor : removed else if

--- FindMaximum/Program.cs
+++ FindMaximum/Program.cs
@@ -37,22 +37,22 @@ namespace FindMaximum
         //static int findMaximum(int num1, int num2, int num3)
         //{
         //    if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0) return num1;
-        //    else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0) return num2;
-        //    else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0) return num3;
+        //    if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0) return num2;
+        //    if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0) return num3;
         //    else throw new Exception("Values are same");
         //}
         //static float findMaximum(float num1, float num2, float num3)
         //{
         //    if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0) return num1;
-        //    else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0) return num2;
-        //    else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0) return num3;
+        //    if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0) return num2;
+        //    if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0) return num3;
         //    else throw new Exception("Values are same");
         //}
         //static string findMaximum(string input1, string input2, string input3)
         //{
         //    if (input1.CompareTo(input2) > 0 && input1.CompareTo(input3) > 0) return input1;
-        //    else if (input2.CompareTo(input1) > 0 && input2.CompareTo(input3) > 0) return input2;
-        //    else if (input3.CompareTo(input1) > 0 && input3.CompareTo(input2) > 0) return input3;
+        //    if (input2.CompareTo(input1) > 0 && input2.CompareTo(input3) > 0) return input2;
+        //    if (input3.CompareTo(input1) > 0 && input3.CompareTo(input2) > 0) return input3;
         //    else throw new Exception("Values are same");
         //}
         static T findMaximum<T>(T input1, T input2, T input3) where T : IComparable<T>
