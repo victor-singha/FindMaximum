a4e614553cc194d71f6a63aad96a899451376a17
victor-singha <vicunite2@gmail.com>
2021-05-05 09:26:55 +0530

    [victor] generic class added

---
+++ FindMaximum/FindMax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    class FindMax<T> where T : IComparable<T>
+    {
+        private T input1;
+        private T input2;
+        private T input3;
+        public FindMax(T input1, T input2, T input3) 
+        {
+            this.input1 = input1;
+            this.input2 = input2;
+            this.input3 = input3;
+        }
+        public string findMaximum()
+        {
+            if (input1.CompareTo(input2) > 0 && input1.CompareTo(input3) > 0) return input1.ToString();
+            else if (input2.CompareTo(input1) > 0 && input2.CompareTo(input3) > 0) return input2.ToString();
+            else if (input3.CompareTo(input1) > 0 && input3.CompareTo(input2) > 0) return input3.ToString();
+            else throw new Exception("Values are same");
+        }
+    }
+}
--- FindMaximum/Program.cs
+++ FindMaximum/Program.cs
@@ -10,11 +10,11 @@ namespace FindMaximum
     {
         static void Main(string[] args)
         {
-
             //int maxInt = findMaximum(1, 2, 3);
             //float maxFloat = findMaximum(1.1f, 9.4f, 5.7f);
             //string maxString = findMaximum("Apple", "Peach", "Banana");
            
+            //GENERIC METHODS
             string maxInt = findMaximum<int>(1, 2, 3);
             string maxFloat = findMaximum<float>(1.1f, 9.4f, 5.7f);
             string maxString = findMaximum<string>("Apple", "Peach", "Banana");
@@ -23,30 +23,38 @@ namespace FindMaximum
             Console.WriteLine(maxFloat);
             Console.WriteLine(maxString);
 
-            Console.ReadKey();
+            //GENERIC CLASS
+            string maxIntc = new FindMax<int>(3, 4, 9).findMaximum();
+            string maxDoublec = new FindMax<double>(3.6, 4.9, 5.7).findMaximum();
+            string maxStringc = new FindMax<string>("apple","peach","orange").findMaximum();
             
+            Console.WriteLine(maxIntc);
+            Console.WriteLine(maxDoublec);
+            Console.WriteLine(maxStringc);
+
+            Console.ReadKey();          
         }
-        static int findMaximum(int num1, int num2, int num3)
-        {
-            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0) return num1;
-            else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0) return num2;
-            else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0) return num3;
-            else throw new Exception("Values are same");
-        }
-        static float findMaximum(float num1, float num2, float num3)
-        {
-            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0) return num1;
-            else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0) return num2;
-            else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0) return num3;
-            else throw new Exception("Values are same");
-        }
-        static string findMaximum(string input1, string input2, string input3)
-        {
-            if (input1.CompareTo(input2) > 0 && input1.CompareTo(input3) > 0) return input1;
-            else if (input2.CompareTo(input1) > 0 && input2.CompareTo(input3) > 0) return input2;
-            else if (input3.CompareTo(input1) > 0 && input3.CompareTo(input2) > 0) return input3;
-            else throw new Exception("Values are same");
-        }
+        //static int findMaximum(int num1, int num2, int num3)
+        //{
+        //    if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0) return num1;
+        //    else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0) return num2;
+        //    else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0) return num3;
+        //    else throw new Exception("Values are same");
+        //}
+        //static float findMaximum(float num1, float num2, float num3)
+        //{
+        //    if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0) return num1;
+        //    else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0) return num2;
+        //    else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0) return num3;
+        //    else throw new Exception("Values are same");
+        //}
+        //static string findMaximum(string input1, string input2, string input3)
+        //{
+        //    if (input1.CompareTo(input2) > 0 && input1.CompareTo(input3) > 0) return input1;
+        //    else if (input2.CompareTo(input1) > 0 && input2.CompareTo(input3) > 0) return input2;
+        //    else if (input3.CompareTo(input1) > 0 && input3.CompareTo(input2) > 0) return input3;
+        //    else throw new Exception("Values are same");
+        //}
         static string findMaximum<T>(T input1, T input2, T input3) where T : IComparable<T>
         {
             if (input1.CompareTo(input2) > 0 && input1.CompareTo(input3) > 0) return input1.ToString();
@@ -54,7 +62,5 @@ namespace FindMaximum
             else if (input3.CompareTo(input1) > 0 && input3.CompareTo(input2) > 0) return input3.ToString();
             else throw new Exception("Values are same");
         }
-
-
     }
 }
